d1e2867b6c864c47446ac8ab39e1e92eb173b732
Dimitris <georgiou_dimitris@outlook.com>
2024-11-06 08:24:23 +0200

    First Deploy

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
 import fastapi as _fastapi
 import service as _service
-import starlette.responses as _responses
 
 app = _fastapi.FastAPI()
-
-
-@app.get("/")
-async def root():
-    return _responses.RedirectResponse("/redoc")
 
 
 @app.get("/events/today")
